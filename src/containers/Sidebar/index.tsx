46ad0644d0b5b2270d75bd7f5cbfd963b0c56ac7
Edmar <edmar.contato149@gmail.com>
2024-02-16 08:16:21 -0300

    criando cada pasta para cada pedaço

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/EdmarAugust0.png" alt="Avatar" />
+    <Titulo>Edmar Augusto</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+      voluptates voluptatum officia non expedita laborum placeat nulla omnis
+      tempore voluptas dolores nisi necessitatibus sapiente tempora itaque, id,
+      adipisci sint rem!
+    </p>
+  </section>
+)
+
+export default Sobre
